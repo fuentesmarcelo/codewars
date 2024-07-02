3b382f93cdf0cacb4b8801a1621e33a6b480bce1
Marcelo <mfuentes1205@gmail.com>
2024-07-02 16:56:25 -0700

    adding five to the parameter and return the new value

--- 8kyu/basic-function-fixer.js
+++ 8kyu/basic-function-fixer.js
@@ -1,4 +1,9 @@
+// PREP 2024-07-02
 // I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+// P: num
+//R: add five to the num
+
 
 // my solutions 
 function addFive(num) {
