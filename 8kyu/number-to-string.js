63e101302bf6efa81010b79989c8e178b625caf7
Marcelo <mfuentes1205@gmail.com>
2024-07-26 14:30:32 -0700

    new changes for July 26

---
+++ 8kyu/number-to-string.js
@@ -0,0 +1,15 @@
+// We need a function that can transform a number (integer) into a string.
+
+function numberToString(num) {
+    // Return a string of the number here!
+}
+
+// my solutions
+function numberToString(num) {
+    let str = num.toString();
+    return str;
+}
+
+function numberToString(num){
+    return num.toString();
+}
---
+++ 8kyu/string-to-number.js
@@ -0,0 +1,17 @@
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+const stringToNumber = function(str){
+    // put your code here
+    return null;
+}
+
+
+// my solutions
+const stringToNumber = function(str){
+    let num = Number(str);
+    return num;
+}
+
+var stringToNumber = function(str){
+    return parseInt(str);
+}
