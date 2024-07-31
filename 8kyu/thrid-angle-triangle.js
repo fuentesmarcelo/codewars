a73a67ce6c0bb23de2f39cd490133a03ea416e9a
Marcelo <mfuentes1205@gmail.com>
2024-07-31 12:52:08 -0700

    adding daily codewars solution

---
+++ 8kyu/thrid-angle-triangle.js
@@ -0,0 +1,18 @@
+// You are given two interior angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+// Note: only positive integers will be tested.
+
+
+function otherAngle(a, b) {
+    return 0;
+}
+
+
+// my solutions
+
+function otherAngle(a, b) {
+    let c = 180 - (a + b);
+    return c;
+}
