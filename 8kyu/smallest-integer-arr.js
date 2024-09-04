567c87bd3d70cc928d208f7d53cc0bc9db4c6360
Marcelo <mfuentes1205@gmail.com>
2024-09-04 13:24:59 -0700

    finding the smallest integer of an array of numbers

---
+++ 8kyu/smallest-integer-arr.js
@@ -0,0 +1,22 @@
+// PREP
+
+// Given an array of integers your solution should find the smallest integer.
+
+function findSmallestInt(arr) {
+    //your code here
+    return 0;
+}
+
+// P: the parameter is an array of integers
+// R: we want to return the smallest integer
+// E: Given [34, 15, 88, 2] your solution will return 2
+// P: we will first need to declare a sortedNums variable using .sort
+// once we have the numbers sorted we can call using the 0 index
+
+
+// my solution
+
+function findSmallestInt(arr) {
+    let sortedNums = arr.sort((a, b) => a - b)
+    return sortedNums[0];
+}
