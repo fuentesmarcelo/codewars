d70303c0ca6e669102900e9eb2d4a061c14e408c
Marcelo <mfuentes1205@gmail.com>
2024-09-15 16:58:00 -0700

    returning the sum of an array of all the numbers, not strings

---
+++ 8kyu/sum-mixed-array.js
@@ -0,0 +1,24 @@
+// PREP
+
+//Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+//Return your answer as a number.
+
+function sumMix(x){
+    //code here
+}
+
+// P: the parameter is an array of integers as strings and numbers
+// R: we want to return the sum of the array values if all were numbers
+// E: n/a
+// P: try to solve using an arrow function
+// they to use .reduce to solve the problem and test
+
+
+// my solution
+
+function sumMix(x){
+    let sum = 0;
+    for(let i = 0; i < x.lengthl i++){
+        return sum += x[i];
+    }
+}
