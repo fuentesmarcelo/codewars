e84a6581b83be9c127ab70cb944117bcc1b207e5
Marcelo <mfuentes1205@gmail.com>
2024-09-16 11:02:47 -0700

    combining two strings using only new ES6

---
+++ 8kyu/join-strings.js
@@ -0,0 +1,20 @@
+// PREP
+
+// ES6 has introduced another way of joining strings. Your task is to find out what this is and write a function that will add two strings together, there must be a space between the two strings.
+// + , .concat() & .join() will not be allowed in this exercise. I have also removed some other methods that can be used to cheat!
+
+function joinStrings(string1, string2){
+    // code here
+}
+
+// P: the parameter are two strings
+// R: we want to return the strings combined without using +, concat, or join and only using new ES6 
+// E: n/a
+// P: use back ticks and money sign with curley
+
+
+// my solution 
+
+function joinStrings(string1, string2){
+    return `${string1} ${string2}`
+}
