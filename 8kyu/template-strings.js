2adbc23b464674b8adbfa19d0b0a2a6d847efd6c
Marcelo <mfuentes1205@gmail.com>
2024-09-17 15:04:32 -0700

    solving for template literals using the new back ticks

---
+++ 8kyu/template-strings.js
@@ -0,0 +1,19 @@
+// PREP
+
+// Your task is to return the correct string using the Template String Feature.
+
+var templateStrings = function(noun, adjective) {
+    return //complete this
+}
+
+// P: the parameter is a noun and adjective
+// R: we using template literals
+// E: n/a
+// P: use back ticks
+
+
+// my solution
+
+var templateStrings = function(noun, adjective) {
+    return `${noun} are ${adjective}`
+}
