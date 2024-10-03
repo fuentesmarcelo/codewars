54cb7f33f3407738f5016ab010a0c23c9844819f
Marcelo <mfuentes1205@gmail.com>
2024-10-03 16:57:20 -0700

    returning the sum of the elements being all positive and all negative

---
+++ 8kyu/pos-sum-neg.js
@@ -0,0 +1,35 @@
+// // Given an array of integers.
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+// If the input is an empty array or is null, return an empty array.
+
+function countPositivesSumNegatives(input) {
+    // your code here
+}
+
+
+// the parameter is an array of integers
+// return the array with the first element beings only sum of positive numbers and the second element being the sum of negative numbers
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+
+
+
+
+// my solution
+
+function countPositivesSumNegatives(input) {
+    if (input == null || input.length == 0)
+      return [];
+    
+    var positive = 0;
+    var negative = 0;
+    
+    for (var i=0, l=input.length; i<l; ++i)
+    {
+      if (input[i] > 0)
+        ++ positive;
+      else
+        negative += input[i];
+    }
+    
+    return [positive, negative];
